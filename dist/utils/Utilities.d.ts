8e05c5c8451a002849d170747555443311e681ef
SOOS-JAlvarez <jalvarez@soos.io>
2023-10-20 15:49:58 -0300

    removed dist added to gitignore.

--- dist/utils/Utilities.d.ts
+++
@@ -1,3 +0,0 @@
-import { EnvironmentEnum } from "../soosApiClient";
-export declare function isNil(value: unknown): value is null | undefined;
-export declare function constructBaseApiUrl(environment: EnvironmentEnum, api: string): string;
